62ff420c1af03f638f70e2eed06c5794fe805232
Ulf <ulf.berglund84@gmail.com>
2026-01-07 16:21:46 -0500

    Add message if no active hosts are found

--- main.py
+++ main.py
@@ -4,5 +4,7 @@ if __name__ == "__main__":
     network = input("Enter network range (e.g: 192.168.1.0/24): ")
     hosts = ping_sweep(network)
     print("\nActive hosts:")
+    if len(hosts) == 0:
+        print("No active hosts found.")
     for host in hosts:
         print(host)
