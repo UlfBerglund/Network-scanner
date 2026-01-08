8b8c4c30fc2832b60833767f82c0ad5bd6b6bbf2
Ulf <ulf.berglund84@gmail.com>
2026-01-08 07:38:32 -0500

    Sort open TCP ports numerically before returning results

---
+++ scanner/port_scan.py
@@ -0,0 +1,20 @@
+import nmap
+
+def port_scan(host):
+    """
+    Scans a host for open TCP-ports
+    and returns a list of port numbers.
+    """
+    nm = nmap.PortScanner()
+    try:
+        nm.scan(hosts=host, arguments="-sT")
+    except Exception:
+        return []
+    open_ports = []
+
+    for proto in nm[host].all_protocols():
+        for port in nm[host][proto].keys():
+            if nm[host][proto][port]['state'] == 'open':
+                open_ports.append(port)
+    open_ports.sort()
+    return open_ports
