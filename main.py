d111c8a0cf8b1d995e678bf03d4664602cacc404
Ulf <ulf.berglund84@gmail.com>
2026-01-08 07:06:03 -0500

    Always show host and show when no open TCP ports are found

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from scanner.ping_sweep import ping_sweep
+from scanner.port_scan import port_scan
 
 if __name__ == "__main__":
     network = input("Enter network range (e.g: 192.168.1.0/24): ")
@@ -7,4 +8,10 @@ if __name__ == "__main__":
     if len(hosts) == 0:
         print("No active hosts found.")
     for host in hosts:
-        print(host)
+        open_ports = port_scan(host)
+        if len(open_ports) > 0:
+            print(host)
+            print(f" Open ports: {', '.join(map(str, open_ports))}")
+        else:
+            print(" No open TCP ports found")
+            
